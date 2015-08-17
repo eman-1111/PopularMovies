057bd2d0c4f60d91dd9231b46dcbcddf030a96df
eman-1111 <eman.ashour1111@gmail.com>
2015-08-17 18:31:33 +0300

    touch_selector &  sort order

--- app/src/main/java/app/movies/eman/popularmovies/MainActivity.java
+++ app/src/main/java/app/movies/eman/popularmovies/MainActivity.java
@@ -104,23 +104,6 @@ public class MainActivity extends ActionBarActivity implements MoviesFragment.Ca
         mSortBy = sortBy;
 
     }
-    //    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        String location = Utility.getPreferredLocation( this );
-//        // update the location in our second pane using the fragment manager
-//        if (location != null && !location.equals(mLocation)) {
-//            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
-//            if ( null != ff ) {
-//                ff.onLocationChanged();
-//            }
-//            DetailFragment df = (DetailFragment)getSupportFragmentManager().findFragmentByTag(DETAILFRAGMENT_TAG);
-//            if ( null != df ) {
-//                df.onLocationChanged(location);
-//            }
-//            mLocation = location;
-//        }
-//    }
 
 
 }
